204343d8fd83707d4292776795e2ce4726b0cee3
gino lopez <gmlm_120@hotmail.com>
2017-04-25 11:35:03 -0400

    actualizacion

---
+++ app/Models/Service_Accommodation.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Service_Accommodation extends Model
+{
+     protected $cod_service=['id'];
+     protected $table = 'service_accommodation';
+     public $timestamps = false;
+}
